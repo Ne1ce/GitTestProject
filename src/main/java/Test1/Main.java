0f7754df871bfa7e47c9a76725323204ab063c52
Nikolay K <113373360+Ne1ce@users.noreply.github.com>
2022-11-28 11:29:41 +0300

    Initial commit

---
+++ src/main/java/Test1/Main.java
@@ -0,0 +1,11 @@
+package Test1;
+
+/**
+ * @created ${DAY}/${MONTH}/${YEAR} - ${TIME}
+ * @project ${PROJECT_NAME}
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
